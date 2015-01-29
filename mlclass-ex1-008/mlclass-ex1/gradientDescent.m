0c0ccf9b3b831e9c50522d58720ec83eb9ae1927
Lu <lujiaying93@foxmail.com>
2015-01-29 15:04:13 +0800

    Programming Exercise (Week 2)
    Linear Regression with Multiple Variables
    多变量的线性回归

---
+++ mlclass-ex1-008/mlclass-ex1/gradientDescent.m
@@ -0,0 +1,39 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+%     tmp_theta1 = theta(1) - alpha * 1/m * sum((X*theta - y).*X(:,1), 1);
+%     tmp_theta2 = theta(2) - alpha * 1/m * sum((X*theta - y).*X(:,2), 1);
+
+%     theta = [tmp_theta1; tmp_theta2];
+    error = X*theta - y;
+    error_matrix = [];
+    for i = 1:size(X, 2)
+        error_matrix = [error_matrix error];
+    end
+    
+    tmp_theta = theta - (alpha * 1/m * sum(error_matrix.*X, 1))';
+    theta = tmp_theta;
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
